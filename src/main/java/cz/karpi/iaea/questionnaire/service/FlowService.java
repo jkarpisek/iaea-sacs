b75fe2234f32cde92a357dc099718aa42e5c432a
svoboda.jm <svoboda.jm@gmail.com>
2017-10-10 17:38:16 +0200

    do menu  nahoře přidat čísla, e.g. 1/9 tlačítka Next&Previous mají přeskočit prázdné sekce u Assessment a Planner

--- src/main/java/cz/karpi/iaea/questionnaire/service/FlowService.java
+++ src/main/java/cz/karpi/iaea/questionnaire/service/FlowService.java
@@ -1,15 +1,21 @@
 package cz.karpi.iaea.questionnaire.service;
 
+import cz.karpi.iaea.questionnaire.model.Element;
+import cz.karpi.iaea.questionnaire.model.Question;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.IntStream;
 
 import cz.karpi.iaea.questionnaire.model.Flow;
 import cz.karpi.iaea.questionnaire.model.SubCategory;
+
+import static cz.karpi.iaea.questionnaire.model.Flow.EFlowType.ASSESSMENT;
+import static cz.karpi.iaea.questionnaire.model.Flow.EFlowType.PLANNER;
 
 /**
  * Created by karpi on 18.4.17.
@@ -67,11 +73,63 @@ public class FlowService {
         getFlow().setCategoryAndSubCategory(categoryIdx, () -> subCategoryIdx);
     }
 
-    private boolean isNotEmpty() {
-        return formService.getCategories().get(getFlow().getCurrentCategoryIndex())
-                .getSubCategories().get(getFlow().getCurrentSubCategoryIndex())
-                .getQuestions().stream().anyMatch(question -> formService.getSacsRows().get(question).getPiGrade() != null);
+    private boolean isNotEmpty(Flow.EFlowType currentFlow) {
+        if (currentFlow == Flow.EFlowType.ASSESSMENT) {
+            return formService.getCategories().get(getFlow().getCurrentCategoryIndex())
+                    .getSubCategories().get(getFlow().getCurrentSubCategoryIndex())
+                    .getQuestions().stream().anyMatch(question -> formService.getSacsRows().get(question).getPiGrade() != null);
+        } else if (currentFlow == Flow.EFlowType.PLANNER) {
+            List<Question> questions = formService
+                    .getCategories()
+                    .get(getFlow().getCurrentCategoryIndex())
+                    .getSubCategories()
+                    .get(getFlow().getCurrentSubCategoryIndex())
+                    .getQuestions();
+            for (Question question: questions) {
+                for (Map.Entry<Element, Integer> piGrade : formService.getAssessmentRow(question).getElementsPiGrade().entrySet()) {
+                    if (piGrade.getValue() != null) return true;
+                }
+            }
+            return false;
+        } else {
+            return true;
+        }
     }
+
+    private void setFlowToNextNonEmptySection(Flow.EFlowType currentFlow) {
+        //skip pages which are "empty"
+        //check whether next subcategory is not "empty"
+        //check whether next category has subcategory which is not "empty"
+        //else getFlow().resetFlow(Flow.EFlowType.PLANNER);
+        while (formService.getCategories().size() >= getFlow().getCurrentCategoryIndex() + 1) {
+            while (getCurrentSubCategory().getCategory().getSubCategories().size() > getFlow().getCurrentSubCategoryIndex() + 1) {
+                getFlow().upSubCategory();
+                if (isNotEmpty(currentFlow))
+                    return;
+            }
+
+            if (formService.getCategories().size() <= getFlow().getCurrentCategoryIndex() + 1) {
+                if (currentFlow == Flow.EFlowType.ASSESSMENT) {
+                    getFlow().resetFlow(Flow.EFlowType.PLANNER);
+                    if (isNotEmpty(currentFlow))
+                        return;
+
+                    //find next non-empty planner
+                    setFlowToNextNonEmptySection(Flow.EFlowType.PLANNER);
+                } else if (currentFlow == Flow.EFlowType.PLANNER) {
+                    getFlow().resetFlow(Flow.EFlowType.CDP);
+                }
+                return;
+            }
+
+            getFlow().upCategory();
+            getFlow().resetSubCategory();
+
+            if (isNotEmpty(currentFlow))
+                return;
+        }
+    }
+
 
     public void moveCounterToNext() {
         switch (getFlow().getFlowType()) {
@@ -82,46 +140,69 @@ public class FlowService {
                 getFlow().resetFlow(Flow.EFlowType.QUESTION);
                 break;
             case QUESTION:
-                resolveCategoryNext(Flow.EFlowType.ASSESSMENT);
+                if (getCurrentSubCategory().getCategory().getSubCategories().size() > getFlow().getCurrentSubCategoryIndex() + 1) {
+                    getFlow().upSubCategory();
+                } else if (formService.getCategories().size() > getFlow().getCurrentCategoryIndex() + 1){
+                    getFlow().upCategory();
+                } else {
+                    getFlow().resetFlow(Flow.EFlowType.ASSESSMENT);
+                    if (isNotEmpty(Flow.EFlowType.ASSESSMENT))
+                        return;
+                    setFlowToNextNonEmptySection(Flow.EFlowType.ASSESSMENT);
+                }
                 break;
             case ASSESSMENT:
-                //I would try to skip pages which are "empty"
-                //check whether next subcategory is not "empty"
-                //check wether next category has subcategory which is not "empty"
-                //else getFlow().resetFlow(Flow.EFlowType.PLANNER);
-                while (formService.getCategories().size() >= getFlow().getCurrentCategoryIndex() + 1) {
-                    while (getCurrentSubCategory().getCategory().getSubCategories().size() > getFlow().getCurrentSubCategoryIndex() + 1) {
-                        getFlow().upSubCategory();
-                        if (isNotEmpty())
-                            return;
-                    }
-
-                    if (formService.getCategories().size() <= getFlow().getCurrentCategoryIndex() + 1) {
-                        getFlow().resetFlow(Flow.EFlowType.PLANNER);
-                        return;
-                    }
-
-                    getFlow().upCategory();
-                    getFlow().resetSubCategory();
-
-                    if (isNotEmpty())
-                        return;
-                }
-                //MS resolveCategoryNext(Flow.EFlowType.PLANNER);
-                //break;
+                setFlowToNextNonEmptySection(Flow.EFlowType.ASSESSMENT);
+                break;
             case PLANNER:
-                resolveCategoryNext(Flow.EFlowType.CDP);
+                setFlowToNextNonEmptySection(Flow.EFlowType.PLANNER);
                 break;
         }
     }
 
-    private void resolveCategoryNext(Flow.EFlowType next) {
-        if (getCurrentSubCategory().getCategory().getSubCategories().size() > getFlow().getCurrentSubCategoryIndex() + 1) {
-            getFlow().upSubCategory();
-        } else if (formService.getCategories().size() > getFlow().getCurrentCategoryIndex() + 1){
-            getFlow().upCategory();
-        } else {
-            getFlow().resetFlow(next);
+    private void setFlowToPreviousNonEmptySection(Flow.EFlowType currentFlow) {
+        while (getFlow().getCurrentCategoryIndex() >= 0) {
+            while (getFlow().getCurrentSubCategoryIndex() - 1 >= 0) {
+                getFlow().downSubCategory();
+                if (isNotEmpty(currentFlow))
+                    return;
+            }
+
+            if (getFlow().getCurrentCategoryIndex() - 1 < 0) {
+                if (currentFlow == Flow.EFlowType.CDP) {
+                    getFlow().resetFlow(Flow.EFlowType.PLANNER);
+                    getFlow().setCategoryAndSubCategory(
+                            formService.getCategories().size() - 1,
+                            () -> formService.getCategories().get(formService.getCategories().size() - 1).getSubCategories().size() - 1
+                    );
+                    setFlowToPreviousNonEmptySection(Flow.EFlowType.PLANNER);
+                }
+                if (currentFlow == Flow.EFlowType.PLANNER) {
+                    getFlow().resetFlow(Flow.EFlowType.ASSESSMENT);
+                    getFlow().setCategoryAndSubCategory(
+                            formService.getCategories().size() - 1,
+                            () -> formService.getCategories().get(formService.getCategories().size() - 1).getSubCategories().size() - 1
+                    );
+                    //find next non-empty planner
+                    setFlowToPreviousNonEmptySection(Flow.EFlowType.ASSESSMENT);
+                } else if (currentFlow == Flow.EFlowType.ASSESSMENT) {
+                    getFlow().resetFlow(Flow.EFlowType.QUESTION);
+                    getFlow().setCategoryAndSubCategory(
+                            formService.getCategories().size() - 1,
+                            () -> formService.getCategories().get(formService.getCategories().size() - 1).getSubCategories().size() - 1
+                    );
+                }
+                return;
+            }
+
+            getFlow().downCategory(() -> getCurrentSubCategory().getCategory().getSubCategories().size() - 1);
+            getFlow().setCategoryAndSubCategory(
+                    getFlow().getCurrentCategoryIndex(),
+                    () -> formService.getCategories().get(getFlow().getCurrentCategoryIndex()).getSubCategories().size() - 1
+            );
+
+            if (isNotEmpty(currentFlow))
+                return;
         }
     }
 
@@ -131,16 +212,23 @@ public class FlowService {
                 getFlow().resetFlow(Flow.EFlowType.START);
                 break;
             case QUESTION:
-                resolveCategoryPrevious(Flow.EFlowType.INSTRUCTION, 0);
+                if (getFlow().getCurrentSubCategoryIndex() > 0) {
+                    getFlow().downSubCategory();
+                } else if (getFlow().getCurrentCategoryIndex() > 0){
+                    getFlow().downCategory(() -> getCurrentSubCategory().getCategory().getSubCategories().size() - 1);
+                } else {
+                    getFlow().resetFlow(Flow.EFlowType.INSTRUCTION);
+                }
                 break;
             case ASSESSMENT:
-                resolveCategoryPrevious(Flow.EFlowType.QUESTION, formService.getCategories().size() - 1);
+                //find previous non empty or jump to the last question
+                setFlowToPreviousNonEmptySection(Flow.EFlowType.ASSESSMENT);
                 break;
             case PLANNER:
-                resolveCategoryPrevious(Flow.EFlowType.ASSESSMENT, formService.getCategories().size() - 1);
+                setFlowToPreviousNonEmptySection(Flow.EFlowType.PLANNER);
                 break;
             case CDP:
-                resolveCategoryPrevious(Flow.EFlowType.PLANNER, formService.getCategories().size() - 1);
+                setFlowToPreviousNonEmptySection(Flow.EFlowType.CDP);
                 break;
         }
     }
